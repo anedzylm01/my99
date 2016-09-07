527ec07d5cbeeefccef774f6011b783eec4f05cb
Edzyl Gu <anedzyl@mobile01.com>
2016-09-07 13:41:40 +0800

    coding style

--- 99.php
+++ 99.php
@@ -1,7 +1,7 @@
 <?php
     for ($i = 1; $i < 10; $i++) {
         for ($j = 1; $j < 10; $j++) {
-            echo "$j*$i=", str_pad($i * $j, 2, ' '), " ";
+            echo "{$j} * {$i} = " . str_pad($i * $j, 2, ' ') . " ";
         }
         echo "\n";
     }
